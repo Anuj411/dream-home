1a1e9ec24b0bec52e369ad26891f638d4d590de6
Anuj <panchalanuj411@gmail.com>
2023-08-26 00:20:57 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home - Dream home</title>
+</head>
+<body>
+    <form action="second.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="filenm" required/><br><br>
+        <input type="submit" name="submit" />
+    </form>
+</body>
+</html>
---
+++ second.php
@@ -0,0 +1,23 @@
+<?php
+    if(isset($_POST["submit"])){
+        $file_name = $_FILES['filenm']['name'];
+        $tmp_dir = $_FILES['filenm']['tmp_name'];
+        move_uploaded_file($tmp_dir, "Uploads/".$file_name);
+    }
+?>
+<html lang="en">
+<head>
+    <title>Second - Dream Home</title>
+</head>
+<body>
+    <h3>Done</h3>
+    <!-- <h4>(Click to download file)</h4> -->
+    <?php
+    //     $path = __DIR__;
+    //     $contents = scandir($path."/Uploads/");
+    //     foreach($contents as $file){
+    //         echo "<a download='$file' href='Uploads/$file'>".$file."</a><br>";
+    //     }
+    ?>
+</body>
+</html>
