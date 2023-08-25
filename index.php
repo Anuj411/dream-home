97e7c45696d4820b5bb5004177ae8286a5b7d023
Anuj <panchalanuj411@gmail.com>
2023-08-26 00:34:29 +0530

    Update index page

--- index.php
+++ index.php
@@ -4,9 +4,18 @@
     <title>Home - Dream home</title>
 </head>
 <body>
-    <form action="second.php" method="POST" enctype="multipart/form-data">
+    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST" enctype="multipart/form-data">
         <input type="file" name="filenm" required/><br><br>
         <input type="submit" name="submit" />
     </form>
 </body>
 </html>
+
+<?php
+if(isset($_POST["submit"])){
+        $file_name = $_FILES['filenm']['name'];
+        $tmp_dir = $_FILES['filenm']['tmp_name'];
+        move_uploaded_file($tmp_dir, "Uploads/".$file_name);
+	echo "Done";
+}
+?>
